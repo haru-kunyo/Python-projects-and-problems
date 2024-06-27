24307bf660b6356266d569df615ad6cc5fb23da3
haru-kunyo <dainxin@gmail.com>
2024-06-27 11:16:44 +0530

    Update goodmorningsir.py

--- goodmorningsir.py
+++ goodmorningsir.py
@@ -2,9 +2,9 @@ import time
 timestamp = int(time.strftime('%H'))
 if timestamp>0 and timestamp<12:
     print('Good Morning, Sir')
-elif timestamp>=12 and timestamp<5:
+elif timestamp>=12 and timestamp<17:
     print('Good Afternoon, Sir')
-elif timestamp>=5 and timestamp<9:
+elif timestamp>=17 and timestamp<21:
     print('Good Evening, Sir')
 else:
     print('Good Night, Sir')
